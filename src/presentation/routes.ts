70d330aaf16e554f0762ccf5b1bdfa67ab3b25bb
DayanaOviedo2001 <108598745+DayanaOviedo2001@users.noreply.github.com>
2023-10-20 00:24:15 -0500

    ruta de paciente agregado

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { DoctorRoutes,  } from './Doctor/routes';
 import { AlergiasRoutes,  } from './Alergias/routes';
 import { CitaRoutes,  } from './Cita/routes';
+import { PacienteRoutes,  } from './Pacientes/routes';
 
 
 
@@ -16,6 +17,7 @@ export class AppRoutes {
     router.use('/api/Doctor', DoctorRoutes.routes );
     router.use('/api/Alergias', AlergiasRoutes.routes );
     router.use('/api/Cita', CitaRoutes.routes );
+    router.use('/api/Paciente', PacienteRoutes.routes);
     return router;
   }
 
