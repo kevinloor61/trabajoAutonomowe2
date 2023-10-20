b8438166bd33a28fc3b7653736a9b51e1f9d29ed
DayanaOviedo2001 <108598745+DayanaOviedo2001@users.noreply.github.com>
2023-10-19 23:25:28 -0500

    agregado dominio pacientes

---
+++ src/domain/dtos/Pacientes/create-Pacientes.dto.ts
@@ -0,0 +1,25 @@
+export class CreatePacienteDto {
+  private constructor(
+    public readonly nombre: string,
+    public readonly apellido: string,
+    public readonly cedula: string,
+    public readonly genero: string,
+    public readonly estado: string,
+    public readonly alergiaId?: number,
+   
+  ){}
+  static create( props: {[key:string]: any} ): [string?, CreatePacienteDto?]  {
+
+    const { nombre, apellido, cedula, genero,estado,alergiaId } = props;
+
+    if ( !nombre ) return ['la propiedad nombre es requerida', undefined];
+    if ( !apellido ) return ['la propiedad apellido es requerida', undefined];
+    if ( !cedula ) return ['la propiedad cedula es requerida', undefined];
+    if ( !genero ) return ['la propiedad genero es requerida', undefined];
+    if ( !estado ) return ['la propiedad estado es requerida', undefined];
+    if ( !alergiaId ) return ['la propiedad alergia es requerida', undefined];
+
+
+    return [undefined, new CreatePacienteDto(nombre, apellido, cedula, genero, estado, alergiaId)];
+  }
+}
---
+++ src/domain/dtos/Pacientes/update-Pacientes.dto.ts
@@ -0,0 +1,42 @@
+export class UpdatePacienteDto {
+
+  private constructor(
+    public readonly id: number,
+    public readonly nombre: string,
+    public readonly apellido: string,
+    public readonly cedula: string,
+    public readonly genero: string,
+    public readonly estado: string,
+    public readonly alergiaId?: number,
+  ){}
+
+  get values() {
+    const returnObj: {[key: string]: any} = {};
+
+    if ( this.id ) returnObj.name = this.id;
+    if ( this.nombre ) returnObj.genre = this.nombre;
+    if ( this.apellido ) returnObj.genre = this.apellido;
+    if ( this.cedula ) returnObj.genre = this.cedula;
+    if ( this.genero ) returnObj.genre = this.genero;
+    if ( this.estado ) returnObj.genre = this.estado;
+
+    return returnObj;
+  }
+
+
+  static create( props: {[key:string]: any} ): [string?, UpdatePacienteDto?]  {
+
+    const { id, nombre, apellido, cedula, correo, genero, estado, alergiaId } = props;
+
+    if ( !id || isNaN( Number(id)) ) {
+      return ['id must be a valid number'];
+    }
+
+    if ( !nombre && !apellido && !cedula && !correo && !genero && !estado && !alergiaId) {
+      return ['At least one property must be provided'];
+    }
+    return [undefined, new UpdatePacienteDto(id, nombre, apellido, cedula, genero, estado, alergiaId )];
+  }
+
+
+}
