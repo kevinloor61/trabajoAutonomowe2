650f247db3e99000768fdbe041a1fe51c5427371
Kevin José Loor <94498055+kevinloor61@users.noreply.github.com>
2023-10-23 23:48:40 -0500

    rotas cambiadas
    kevin

--- src/presentation/Pacientes/routes.ts
+++ src/presentation/Pacientes/routes.ts
@@ -5,12 +5,12 @@ export class PacienteRoutes {
   static get routes(): Router {
     const router = Router();
     const pacienteController = new PacienteController();
-    router.get('/', PacienteController.getPaciente);
-    router.get('/:id', PacienteController.getPaciente );
+    router.get('/', pacienteController.getPaciente);
+    router.get('/:id', pacienteController.getPaciente );
     
-    router.post('/', PacienteController.getPaciente );
-    router.put('/:id', PacienteController.updatePaciente );
-    router.delete('/:id', PacienteController.getPaciente );
+    router.post('/', pacienteController.getPaciente );
+    router.put('/:id', pacienteController.updatePaciente );
+    router.delete('/:id', pacienteController.getPaciente );
     return router;
   }
 }
