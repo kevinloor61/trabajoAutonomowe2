34c8ec5ce0865ace256b11dd86557e78390e9109
Kevin José Loor <94498055+kevinloor61@users.noreply.github.com>
2023-10-24 00:10:51 -0500

    ...

--- src/presentation/Pacientes/controller.ts
+++ src/presentation/Pacientes/controller.ts
@@ -4,12 +4,6 @@ import { CreatePacienteDto, UpdatePacienteDto } from '../../domain/dtos';
 
 
 export class PacienteController {
-  static updatePaciente(arg0: string, updatePaciente: any) {
-      throw new Error('Method not implemented.');
-  }
-  static getPaciente(arg0: string, getPaciente: any) {
-      throw new Error('Method not implemented.');
-  }
   //* DI
   constructor() { }
   public getPaciente = async( req: Request, res: Response ) => {
@@ -22,7 +16,7 @@ export class PacienteController {
 
   public getPacienteById = async( req: Request, res: Response ) => {
     const id = +req.params.id;
-    //    localhost:3000/Doctor/1
+    //    localhost:3000/Paciete/1
     if ( isNaN( id ) ) return res.status( 400 ).json( { error: 'ID argument is not a number' } );
 
     const paciente = await prisma.paciente.findFirst({
@@ -61,7 +55,7 @@ export class PacienteController {
       where: { id }
     });
     if ( !paciente ) return res.status( 404 ).json( { error: `Paciente with id ${ id } not found` } );
-    const updatePaciente = await prisma.doctor.update({
+    const updatePaciente = await prisma.paciente.update({
       where: { id },
       data: updatePacienteDto!.values
     });
@@ -71,11 +65,11 @@ export class PacienteController {
 
   public deletePaciente = async(req:Request, res: Response) => {
     const id = +req.params.id;
-    const Paciente = await prisma.paciente.findFirst({
+    const paciente = await prisma.paciente.findFirst({
       where: { id }
     });
 
-    if ( !Paciente ) return res.status(404).json({ error: `Paciente with id ${ id } not found` });
+    if ( !paciente ) return res.status(404).json({ error: `Paciente with id ${ id } not found` });
     const deleted = await prisma.paciente.delete({
       where: { id }
     });
